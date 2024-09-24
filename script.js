9f0b34b6e25a3e659ff32fc9189b3f90b1872bba
quentinmathieu <quentin.mth@gmail.com>
2024-09-24 14:51:47 +0200

    working ?

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 let authors, categories, datas;
 let mainContainer = document.querySelector("#container");
-// let defaultImg =  new Image();
-// defaultImg.crossOrigin = "anonymous";
-// defaultImg.src =  'https://p1.storage.canalblog.com/14/48/1145642/91330992_o.png?';
 
 async function loadMainFrame(){
 // Define the API endpoint
@@ -16,10 +13,13 @@ fetch(apiUrl)
     return response.json(); // Parse the JSON response
   })
   .then(async (data) => {
+
     const loader = mainContainer.querySelector(".loader");
     // display all datas
     var count = 0;
     let total = Object.keys(data).length;
+    
+
     for (let index in data){
         // break;
         const loadOne = async () => {
@@ -27,23 +27,22 @@ fetch(apiUrl)
             const book = data[index];
             let bookCard = document.createElement("article");
 
-            // bookPic = new Image();
-            // bookPic.crossOrigin = "anonymous";
-            // bookPic.addEventListener("load", imageReceived, false);
+            let bookPic = document.createElement('img');
+            bookPic.src = book.thumbnailUrl;
+            document.body.appendChild(bookPic);
             
-            // // bookPic.setAttribute('crossOrigin', 'anonymous');
             let bookTitle= document.createElement("h3");
             // let bookISBN, bookDate, bookNbPages, bookDescription= document.createElement("p");
             
             bookTitle.innerHTML = book['title'];
-            // if (book['thumbnailUrl'].length > 0){
-            //     bookPic.src =  'https://p1.storage.canalblog.com/14/48/1145642/91330992_o.png?';
-            // }
-            // else{
-            //     bookPic.src = book['thumbnailUrl']+"?test";
-            // }
+            if (typeof(book['thumbnailUrl']) != 'undefined' && book['thumbnailUrl'].length > 0){
+                bookPic.src = book['thumbnailUrl'];
+            }
+            else{
+                bookPic.src =  'https://p1.storage.canalblog.com/14/48/1145642/91330992_o.png?';
+            }
     
-            // bookCard.appendChild(bookPic);
+            bookCard.appendChild(bookPic);
             bookCard.appendChild(bookTitle);
             // bookCard.appendChild(bookISBN);
             // bookCard.appendChild(bookDate);
@@ -55,7 +54,7 @@ fetch(apiUrl)
             window.scrollTo(0, document.body.scrollHeight);
         }
         // loadOne();
-        const myTimeout = setTimeout(async ()=>{
+        setTimeout(async ()=>{
             loadOne();
             await loadOne;
             if (count == total){
@@ -64,10 +63,8 @@ fetch(apiUrl)
                 }
             }
         }
-        , 10*index);
-    }
-    // delete the loader (spinner) after loading & displaying all datas)
-  })
+        , 1*index);
+    }  })
   .then(
     
   )
